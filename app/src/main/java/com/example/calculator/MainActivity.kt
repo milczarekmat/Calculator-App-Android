673f686d6b84b368ec33a8b5b3080ccfbbe290f2
Mateusz Milczarek <milczary.mm@gmail.com>
2024-03-30 09:51:56 +0100

    Added simple calculator activity

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.example.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        val simpleCalculatorBtn = findViewById<Button>(R.id.simpleCalculatorBtn)
+
+        simpleCalculatorBtn.setOnClickListener {
+            val intent = Intent(this, SimpleCalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        val exitBtn = findViewById<Button>(R.id.exitBtn)
+
+        exitBtn.setOnClickListener {
+            finishAffinity()
+            exitProcess(0)
+        }
     }
 }
---
+++ app/src/main/java/com/example/calculator/SimpleCalculatorActivity.kt
@@ -0,0 +1,235 @@
+package com.example.calculator
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calculator.databinding.ActivitySimpleCalculatorBinding
+
+class SimpleCalculatorActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySimpleCalculatorBinding
+
+    private var isOperatorInserted = false
+    private var isInitState = true
+    private var firstOperand: Double = 0.0
+    private var clearFlag = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySimpleCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+
+        setUpListeners()
+    }
+
+    private fun handleDigitClick(digit: String) {
+        if (clearFlag) {
+            binding.calculatorResultTV.text = ""
+            clearFlag = false
+//            isOperatorInserted = false
+        }
+        binding.calculatorResultTV.append(digit)
+//        binding.calculatorOperatorTV.text = "" TODO usuwanie znaku dopiero po wykonaniu equals
+    }
+
+//    TODO usuwanie resultTV.text po kliknięciu cyfry w trakcie wyswietlania wyniku
+
+    private fun handleOperatorClick(operator: String) {
+        if (isInitState && binding.calculatorResultTV.text.isEmpty()) {
+            return
+        }
+
+        if (isOperatorInserted) {
+            if (binding.calculatorResultTV.text.isNotEmpty()) { // evaluate expression and insert new operator
+//                TODO warunek if fix
+                handleEqualsClick()
+                insertOperator(operator)
+                firstOperand = binding.calculatorResultTV.text.toString().toDouble()
+            } else { // replace operator
+                val currentOperator = binding.calculatorOperatorTV.text.toString()
+                binding.calculatorOperatorTV.text =
+                    currentOperator.replace(currentOperator.last(), operator[0])
+            }
+        } else {
+            insertOperator(operator)
+            clearAndSaveOperand()
+        }
+    }
+
+    private fun insertOperator(operator: String) {
+        binding.calculatorOperatorTV.append(operator)
+
+        isOperatorInserted = true
+        isInitState = false
+        clearFlag = true
+    }
+
+    private fun clearAndSaveOperand() {
+        if (binding.calculatorResultTV.text.isEmpty()) {
+            return
+        }
+
+        firstOperand = binding.calculatorResultTV.text.toString().toDouble()
+//        binding.calculatorResultTV.text = ""
+    }
+
+    private fun handleDecimalClick() {
+        val currentText = binding.calculatorResultTV.text.toString()
+        if (currentText.isEmpty() || isOperatorInserted) {
+            binding.calculatorResultTV.append("0.")
+        } else {
+            binding.calculatorResultTV.append(".")
+        }
+    }
+
+    private fun handleBackSpaceClick() {
+        val resultTV = binding.calculatorResultTV
+        if (resultTV.text.isNotEmpty()) {
+            binding.calculatorResultTV.text = resultTV.text.substring(0, resultTV.text.length - 1)
+            if (resultTV.text.isNotEmpty() && resultTV.text.toString().last() == '.') {
+                resultTV.text = resultTV.text.substring(0, resultTV.text.length - 1)
+            }
+        }
+
+        if (resultTV.text.isEmpty()) {
+            isInitState = true
+        }
+    }
+
+    private fun handleSignClick() {
+        val currentText = binding.calculatorResultTV.text.toString()
+        if (currentText.isNotEmpty()) {
+            val firstChar = currentText[0]
+            if (firstChar == '-') {
+                binding.calculatorResultTV.text = currentText.substring(1)
+            } else {
+                binding.calculatorResultTV.text = "-$currentText"
+            }
+        }
+    }
+
+    private fun evaluateExpression(
+        firstOperand: Double,
+        secondOperand: Double,
+        operator: String
+    ): Double {
+        return when (operator) {
+            "+" -> firstOperand + secondOperand
+            "-" -> firstOperand - secondOperand
+            "*" -> firstOperand * secondOperand
+            "/" -> firstOperand / secondOperand
+            else -> firstOperand
+        }
+    }
+
+    private fun handleEqualsClick() {
+        if (binding.calculatorOperatorTV.text.isEmpty()) {
+            return
+        }
+
+        val secondOperand = binding.calculatorResultTV.text.toString().toDouble()
+
+        val result = evaluateExpression(
+            firstOperand,
+            secondOperand,
+            binding.calculatorOperatorTV.text.toString()
+        )
+
+        binding.calculatorResultTV.text = result.toString()
+        firstOperand = result
+
+        isOperatorInserted = false
+        clearFlag = true
+        binding.calculatorOperatorTV.text = ""
+    }
+
+    private fun setUpListeners() {
+        val CEbtn = findViewById<Button>(R.id.CEbtn)
+
+        binding.oneBtn.setOnClickListener {
+            handleDigitClick("1")
+        }
+
+        binding.twoBtn.setOnClickListener {
+            handleDigitClick("2")
+        }
+
+        binding.threeBtn.setOnClickListener {
+            handleDigitClick("3")
+        }
+
+        binding.fourBtn.setOnClickListener {
+            handleDigitClick("4")
+        }
+
+        binding.fiveBtn.setOnClickListener {
+            handleDigitClick("5")
+        }
+
+        binding.sixBtn.setOnClickListener {
+            handleDigitClick("6")
+        }
+
+        binding.sevenBtn.setOnClickListener {
+            handleDigitClick("7")
+        }
+
+        binding.eightBtn.setOnClickListener {
+            handleDigitClick("8")
+        }
+
+        binding.nineBtn.setOnClickListener {
+            handleDigitClick("9")
+        }
+
+        binding.zeroBtn.setOnClickListener {
+            handleDigitClick("0")
+        }
+
+        binding.addBtn.setOnClickListener {
+            handleOperatorClick("+")
+        }
+
+        binding.subBtn.setOnClickListener {
+            handleOperatorClick("-")
+        }
+
+        binding.multiplyBtn.setOnClickListener {
+            handleOperatorClick("*")
+        }
+
+        binding.divideBtn.setOnClickListener {
+            handleOperatorClick("/")
+        }
+
+        binding.equalsBtn.setOnClickListener {
+            handleEqualsClick()
+        }
+
+        binding.backspaceBtn.setOnClickListener {
+            handleBackSpaceClick()
+        }
+
+        binding.ACbtn.setOnClickListener {
+            binding.calculatorResultTV.text = ""
+            binding.calculatorOperatorTV.text = ""
+            firstOperand = 0.0
+            isOperatorInserted = false
+            isInitState = true
+            clearFlag = false
+        }
+
+        binding.CEbtn.setOnClickListener {
+//            resultTV.text = ""
+        }
+
+        binding.decimalBtn.setOnClickListener {
+            handleDecimalClick()
+        }
+
+        binding.signBtn.setOnClickListener {
+            handleSignClick()
+        }
+    }
+}
